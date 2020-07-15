ad43a86ddbc7ed840cd170bf130ceb2c3bfccaa6
carlos zavala <cazcarlos@hotmail.com>
2020-07-15 16:55:03 -0500

    changing subtitle

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="container mt-4">
       <h1 className="text-center"><strong>"PICO Y PLACA" PREDICTOR</strong></h1>
-      <h3 className="mt-4">On this app you would know if you can or can't take your car for a spin</h3>
+      <h3 className="mt-4">On this app you would know if you can or can't take your car out for a spin</h3>
       <Predictor/>
     </div>
   );
