d733e967be28c32cbcf15bf9cd7f76de0be4fba1
carlos zavala <cazcarlos@hotmail.com>
2020-07-14 21:14:57 -0500

    cleaning app and adding bootstrap for styles

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1><strong>"PICO Y PLACA" PREDICTOR</strong></h1>
+      <h3>On this app you would know if you can or can't take your car for a spin</h3>
+      <form>
+
+      </form>
+    </div>
+  );
+}
+
+export default App;
