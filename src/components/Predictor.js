e093055a282036357b26ff45dd0917bbe88e2f8b
carlos zavala <cazcarlos@hotmail.com>
2020-07-15 17:59:16 -0500

    Adding conditional cases to conditionalResponse function

--- src/components/Predictor.js
+++ src/components/Predictor.js
@@ -6,14 +6,15 @@ const Predictor = () => {
     const[plateNumber , setplateNumber] = useState("");
     const[date , setDate] = useState("")
     const[hour , setHour] = useState("")
-    const[calculo , setCalculo] = useState(false);
+    const[callToRender , setcallToRender] = useState(false);
 
 
     const handleSubmit = (event)=>{
         event.preventDefault();
         plateLastDigit();
         extracDay();
-        setCalculo(true);
+        setHour(hour);
+        setcallToRender(true);
     }
 
     const plateLastDigit = ()=>{
@@ -23,8 +24,7 @@ const Predictor = () => {
     }
     
     const extracDay = ()=>{
-      let pattern = /(\d{2})\.(\d{2})\.(\d{4})/;
-        let dt = new Date(date.replace(pattern, "$3-$2-$1"));
+        let dt = new Date(date);
         let day = dt.getDay();
         return setDate(day);
         
@@ -32,19 +32,31 @@ const Predictor = () => {
     console.log("numero de matricula:" , plateNumber , " fecha" , date , " hour ", hour);
 
     const conditionalResponse = ()=>{
-      console.log("Dentro de la funcion conditionalResponse: ", plateNumber, " ", date);
-      if(plateNumber === "3" && date === 1){
-        return document.getElementById("response").innerHTML = "Is tusday, you can't take your car for a spin"
+      console.log("Dentro de la funcion conditionalResponse: ", plateNumber, " ", date, "",hour);
+      if(hour === "7:00" || hour === "9:30" || hour === "16:00" || hour === "19:30" ){
+        if(plateNumber === "1" && date === 0 || plateNumber === "2" && date === 0 ){
+          return document.getElementById("response").innerHTML = "Is Monday, you can't take your car for a spin because you have pico y placa from 7:00 AM to 9:30 AM and 16:00 PM to 19:30PM"
+        }else if(plateNumber === "3" && date === 1 || plateNumber === "4" && date === 1 ){
+          return document.getElementById("response").innerHTML = "Is Tuesday, you can't take your car for a spin because you have pico y placa from 7:00 AM to 9:30 AM and 16:00 PM to 19:30PM"
+        }else if(plateNumber === "5" && date === 2 || plateNumber === "6" && date === 2 ){
+          return document.getElementById("response").innerHTML = "Is Wednesday, you can't take your car for a spin because you have pico y placa from 7:00 AM to 9:30 AM and 16:00 PM to 19:30PM"
+        }else if(plateNumber === "7" && date === 3 || plateNumber === "8" && date === 3 ){
+          return document.getElementById("response").innerHTML = "Is Thursday, you can't take your car for a spin because you have pico y placa from 7:00 AM to 9:30 AM and 16:00 PM to 19:30PM"
+        }else if(plateNumber === "9" && date === 4 || plateNumber === "0" && date === 4 ){
+          return document.getElementById("response").innerHTML = "Is Friday, you can't take your car for a spin because you have pico y placa from 7:00 AM to 9:30 AM and 16:00 PM to 19:30PM"
+        }else{
+          return document.getElementById("response").innerHTML = "You are free to take your car for a spin"
+        }
       }else{
-        return document.getElementById("response").innerHTML = "you can take your car for a spin"
+        return document.getElementById("response").innerHTML = "You are free to take your car for a spin"
       }
     }
 
     useEffect(() => {
-      if(calculo){
+      if(callToRender){
         setTimeout(()=>{
           conditionalResponse();
-        },1000)
+        },1000);
       }
      });
 
