fb310d9711f04c030a407158e2e7eb9c4ebeb579
carlos zavala <cazcarlos@hotmail.com>
2020-07-14 22:05:01 -0500

    Adding the prediction component to the app component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './App.css';
+import Predictor from './components/Predictor';
 
 function App() {
   return (
-    <div className="container">
-      <h1><strong>"PICO Y PLACA" PREDICTOR</strong></h1>
-      <h3>On this app you would know if you can or can't take your car for a spin</h3>
-      <form>
-
-      </form>
+    <div className="container mt-4">
+      <h1 className="text-center"><strong>"PICO Y PLACA" PREDICTOR</strong></h1>
+      <h3 className="mt-4">On this app you would know if you can or can't take your car for a spin</h3>
+      <Predictor/>
     </div>
   );
 }
