50ff099e3cce3aff9512259719085871caddedf1
carlos zavala <cazcarlos@hotmail.com>
2020-07-16 13:59:21 -0500

    Updates

---
+++ precache-manifest.1ce58af81cee58220aee8a8b9a25650e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f66bf86639f33250bbd728654b2aa4a8",
+    "url": "/Pico_y_placa_predictor_react/index.html"
+  },
+  {
+    "revision": "0d23dea8b3f78b6868d7",
+    "url": "/Pico_y_placa_predictor_react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1030c0b02bae90b1103c",
+    "url": "/Pico_y_placa_predictor_react/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "0d23dea8b3f78b6868d7",
+    "url": "/Pico_y_placa_predictor_react/static/js/2.dbdf4a00.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Pico_y_placa_predictor_react/static/js/2.dbdf4a00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1030c0b02bae90b1103c",
+    "url": "/Pico_y_placa_predictor_react/static/js/main.0d012b48.chunk.js"
+  },
+  {
+    "revision": "10f357dc9526ab492bfb",
+    "url": "/Pico_y_placa_predictor_react/static/js/runtime-main.efe5da0b.js"
+  },
+  {
+    "revision": "2979a2a30ad3b1127f4d516118dd8ab5",
+    "url": "/Pico_y_placa_predictor_react/static/media/trafficPicture.2979a2a3.jpg"
+  }
+]);
